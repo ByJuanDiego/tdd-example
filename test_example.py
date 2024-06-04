3076a03faca881249d7c79c4814bcf34278bad5e
Juan Diego Castro Padilla <juan.castro.p@utec.edu.pe>
2024-06-03 20:16:59 -0500

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,10 +1,13 @@
+from fastapi.testclient import TestClient
 import pytest
-import example
+from example import app
+
+client = TestClient(app)
 
 
 def test_can_call_existing_endpoints_of_the_API():
     try:
-        ret = example.get_coordinates("Lima,Peru")
+        ret = client.get("/getcoordinates/?query=Lima, Peru")
         assert(ret is not None)
     except:
         assert False, "Exception while calling an existing function!"
@@ -12,7 +15,7 @@ def test_can_call_existing_endpoints_of_the_API():
 
 def test_cannot_call_nonexisting_endpoints_of_the_API():
     try:
-        ret = example.get_coordinates("blah blah")
+        ret = client.get("/getcoordinates/?query=blah blah")
         assert False, "Exception not raised"
     except:
         pass
@@ -20,7 +23,8 @@ def test_cannot_call_nonexisting_endpoints_of_the_API():
 
 def test_the_results_is_correct_for_simple_cases():
     try:
-        ret = example.get_coordinates("Lima,Peru")
+        ret = client.get("/getcoordinates/?query=Lima, Peru")
+        ret = ret.json()
         expected = {'latitude': '-12.0621065', 'longitude': '-77.0365256'}
         assert ret == expected, "The result is not correct"
     except:
@@ -32,16 +36,20 @@ def test_the_results_is_correct_for_simple_cases():
     ("London", '51.5074', '-0.1278'),
     ("New York", '40.7128', '-74.0060'),
     ("Paris", '48.8566', '2.3522'),
-    ("Tokyo", '35.6895', '139.6917')
+    ("Tokyo", '35.6895', '139.6917'),
+    ('Lima', '-12.0621065', '-77.0365256'),
+    ('Miami', '25.7741728', '-80.19362')
 ])
 def test_the_result_is_correct_for_all_inputs(city_name, expected_lat, expected_lon):
     try:
-        ret = example.get_coordinates(city_name)
+        ret = client.get(f"/getcoordinates/?query={city_name}")
+        ret = ret.json()
+
         expected = {
             'longitude': expected_lon,
             'latitude': expected_lat
         }
-        
+
         assert (abs(float(expected['longitude']) - float(ret['longitude'])) < 1)
         assert (abs(float(expected['latitude']) - float(ret['latitude'])) < 1)
 
