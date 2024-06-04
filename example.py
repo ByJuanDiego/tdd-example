5804ccfb71a7b7e8630e806e6cc09482a759b68b
Juan Diego Castro Padilla <juan.castro.p@utec.edu.pe>
2024-06-03 21:46:40 -0500

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 
 @app.get("/getcoordinates/")
-def get_coordinates(city = "Lima,Perú"):
+def get_coordinates(city):
     """
     Este endpoint obtiene las coordenadas de una ciudad pasada como query.
     Internamente, se comunica con la API de openstreetmap para obtener las coordenadas.
@@ -27,32 +27,18 @@ def get_coordinates(city = "Lima,Perú"):
     }
     response = requests.get(api_url, headers=headers)
     response_data = response.json()
-    
+
     if not response_data:
-        raise Exception(f"There is no coordinate available for the city: {city}")
+        return {
+            'response': 404,
+            'message': 'City not found'
+        }
 
     return {
+        'response': 200,
+        'message': 'Success',
         'latitude': response_data[0]['lat'],
         'longitude': response_data[0]['lon']
     }
 
-
-@app.get("/getdistance/")
-def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float):
-    """
-    Este endpoint calcula la distancia geodésica entre 2 coordenadas
-
-    inputs:
-        float(lat1), float(lon1): La primera coordenada
-        float(lat2), float(lon2): La segunda coordenada
-    output:
-        float(distance): La distancia geodésica entre las 2 coordenadas
-    """
-    coordinates_point_1 = (lat1, lon1)
-    coordinates_point_2 = (lat2, lon2)
-    distance = geodesic(coordinates_point_1, coordinates_point_2).kilometers
-    return distance
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+# run command: uvicorn example:app
